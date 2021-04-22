87db14566b270d40f97706b138127e009e6b8457
Bálint Kiss <kissb2000@gmail.com>
2021-04-22 15:30:05 +0200

    REST API

--- HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -15,16 +15,34 @@ function letöltés() {
         kérdésMegjelenítés(0)
     }
 }
-function kérdésMegjelenítés(k) {
-    let ide_kerdes = document.getElementById("kérdés_szöveg");
-    ide_kerdes.innerHTML = kérdés[k].questionText;
-    console.log('$ {kérdés.length} kérdés érkezett}')
-    for (var i = 1; i < 4; i++) {
-        let elem_kérdés = document.getElementById("válasz" + i)
-        elem_kérdés.innerText = kérdés[k]["answer" + i]
-    }
 
-    document.getElementById("kép2").src = "https://szoft1.comeback.hu/hajo/" + kérdés[k].image
+fetch('/questions/1')
+    .then(response => response.json())
+    .then(data => kérdésMegjelenítés(data)
+);
+
+function kérdésMegjelenítés(k) {
+    console.log(k);
+    document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
+    document.getElementById("válasz1").innerText = kérdés.answer1
+    document.getElementById("válasz2").innerText = kérdés.answer2
+    document.getElementById("válasz3").innerText = kérdés.answer3
+    document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image; 
+}
+
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(válaszfeldolgozás)
+        .then(kérdésMegjelenítés);
+}
+
+function válaszfeldolgozás(válasz) {
+    if (!válasz.ok) {
+        console.error(`Hibás válasz: ${response.status}`)
+    }
+    else {
+        return válasz.json()
+    }
 }
 
 function kattelőre() {
